df58b95471ad730e2163bcd2c1fd8f19737cd438
Ozou87 <oefraty@gmail.com>
2025-11-16 22:40:18 +0200

    flask

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 
-def main():
-    print("Project started successfully!")
+# def main():
+#     print("Project started successfully!")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+
