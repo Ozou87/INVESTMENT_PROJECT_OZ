0d5f66fa32e6bff7ad192107148aa828d34a97ce
Ozou87 <oefraty@gmail.com>
2025-11-23 21:07:57 +0200

    finish growth func

--- algoV1.py
+++ algoV1.py
@@ -1,29 +1,31 @@
 
+#creating table with revenue growth limits and score limits
+GROWTH_THRESHOLDS = [
+    (0.0,20),
+    (5.0,40),
+    (15.0,60),
+    (30.0,80),
+                    ]
+GROWTH_DEFAULT = 95
 
+#generic function that will find growth score and send it to "score_growth" func
+def score_by_thresholds(value, thresholds, default_score):
+    for limit, score in thresholds:
+        if value < limit:
+            return score
+    return default_score
+
+#specific function that sends info to the generic function in order to help it find score
 def score_growth(growth_pct: float) -> int:
-    
-    if growth_pct < 0:
-        return 20
-    elif growth_pct < 5:
-        return 40
-    elif growth_pct < 15:
-        return 60
-    elif growth_pct < 30:
-        return 80
-    else:
-        return 95
+    return score_by_thresholds(growth_pct, GROWTH_THRESHOLDS, GROWTH_DEFAULT)
 
-sc = input("PLS input growth pct: ")
-growth_percentage = float(sc)
-print(score_growth(growth_percentage))
+#let user input annual rev growth
+user_input = input("PLS enter annual revenue growth in %: ")
+#converting the input into a float
+growth_value = float(user_input)
 
-    
-
-# GROWTH_TRSESHHOLDS = [
-#     (0.0,20),
-#     (5.0,40),
-#     (15.0,60),
-#     (30.0,80),
-# ]
-# GROWTH_DEFAULT = 95
+#calling the score growth function and placing the value in "result"
+result = score_growth(growth_value)
+#printing output for user
+print(f"Growth Score is: {result}")
 
